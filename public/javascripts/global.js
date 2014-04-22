dec457b9017bc65606dbfc60bf93b8120ac76a4a
Ian White <ianwhite@utexas.edu>
2014-04-22 14:10:37 -0500

    many changes

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -21,50 +21,50 @@
 //     var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&mode=json&units=imperial";
 
 //     // Dynamically update a banner heading with name of city
-    // $.ajax({
-    //     url: weatherUrl,
-    //     type: "get",
-    //     dataType: "json",
-    //     success: function (data) {
-    //       console.log(data.cod);
-    //       // Only show results if what is typed in city name
-    //       // input is valid
-    //       if (data.cod === 200) {
-    //         $('#cityNameBanner').text(cityName + " Weather");
-    //         $('.container.main').fadeIn(200);
-    //         // Update named fields with API weather data for specific city
-    //         $('#humidity').text(data.main.humidity + "%");
-    //         $('#pressure').text(data.main.pressure);
-    //         $('#temperature').text(data.main.temp);
-    //         $('#high').text(data.main.temp_max);
-    //         $('#low').text(data.main.temp_min);
+//     $.ajax({
+//         url: weatherUrl,
+//         type: "get",
+//         dataType: "json",
+//         success: function (data) {
+//           console.log(data.cod);
+//           // Only show results if what is typed in city name
+//           // input is valid
+//           if (data.cod === 200) {
+//             $('#cityNameBanner').text(cityName + " Weather");
+//             $('.container.main').fadeIn(200);
+//             // Update named fields with API weather data for specific city
+//             $('#humidity').text(data.main.humidity + "%");
+//             $('#pressure').text(data.main.pressure);
+//             $('#temperature').text(data.main.temp);
+//             $('#high').text(data.main.temp_max);
+//             $('#low').text(data.main.temp_min);
 
-            // // Clear city weather input value
-            // $('#cityName').val('');
-            // var weatherCondition = data.weather[0].main
-            // console.log(weatherCondition);
+//             // Clear city weather input value
+//             $('#cityName').val('');
+//             var weatherCondition = data.weather[0].main
+//             console.log(weatherCondition);
 
-            // // Display appropriate Skycon based on current weather.
-            // // May not always work--unsure of all
-            // // possible weather descriptions at the moment
-            // if (weatherCondition.indexOf("Rain") != -1) {
-            //   skycons.add("icon1", Skycons.RAIN);
-            // } else if (weatherCondition.indexOf("Clear") != -1) {
-            //   skycons.add("icon1", Skycons.CLEAR_DAY);
-            // } else if (weatherCondition.indexOf("Wind") != -1) {
-            //   skycons.add("icon1", Skycons.WIND);
-            // } else if (weatherCondition.indexOf("Fog") != -1) {
-            //   skycons.add("icon1", Skycons.FOG);
-            // } else if (weatherCondition.indexOf("Clouds") != -1) {
-            //   skycons.add("icon1", Skycons.CLOUDY);
-            // } else if (weatherCondition.indexOf("Snow") != -1) {
-            //   skycons.add("icon1", Skycons.SNOW);
-            // } else if (weatherCondition.indexOf("Mist") != -1) {
-            //   skycons.add("icon1", Skycons.FOG);
-            // }
+//             // Display appropriate Skycon based on current weather.
+//             // May not always work--unsure of all
+//             // possible weather descriptions at the moment
+//             if (weatherCondition.indexOf("Rain") != -1) {
+//               skycons.add("icon1", Skycons.RAIN);
+//             } else if (weatherCondition.indexOf("Clear") != -1) {
+//               skycons.add("icon1", Skycons.CLEAR_DAY);
+//             } else if (weatherCondition.indexOf("Wind") != -1) {
+//               skycons.add("icon1", Skycons.WIND);
+//             } else if (weatherCondition.indexOf("Fog") != -1) {
+//               skycons.add("icon1", Skycons.FOG);
+//             } else if (weatherCondition.indexOf("Clouds") != -1) {
+//               skycons.add("icon1", Skycons.CLOUDY);
+//             } else if (weatherCondition.indexOf("Snow") != -1) {
+//               skycons.add("icon1", Skycons.SNOW);
+//             } else if (weatherCondition.indexOf("Mist") != -1) {
+//               skycons.add("icon1", Skycons.FOG);
+//             }
 
-            // // Animate Skycons
-            // skycons.play();
+//             // Animate Skycons
+//             skycons.play();
 //           } else {
 //             // Return what was typed into city name input
 //             console.log("Sorry, please enter a city");
