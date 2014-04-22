89984dde16da91831f0c9808d60acdcfcdbef11f
Ian White <ianwhite@utexas.edu>
2014-04-22 11:38:43 -0500

    fix comment line

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -10,7 +10,7 @@ $getWeatherButton.on('click', function(e) {
   $grabCityName.val('');
 });
 
-if we get an "info" emit from the socket server then console.log the data we recive
+// if we get an "info" emit from the socket server then console.log the data we recive
 socket.on('info', function (data) {
     console.log(data);
 });
